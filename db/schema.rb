4b7a151e2872b68debceefd5b8511d09189afe9d
Tim Quimbo <tquimbo1@gmail.com>
2021-12-09 20:08:42 -0500

    test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_24_232317) do
+ActiveRecord::Schema.define(version: 2021_12_08_223023) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,23 @@ ActiveRecord::Schema.define(version: 2021_11_24_232317) do
     t.string "minus"
     t.integer "level"
     t.string "color"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.string "name"
+    t.string "image_url"
+    t.integer "experience"
+    t.integer "level"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "description"
+    t.integer "point"
+    t.string "complete"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
